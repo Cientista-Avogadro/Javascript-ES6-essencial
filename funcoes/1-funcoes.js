3f043c494438676bebce9dac013629707606f538
cientista-avogadro <cientistaavogadro2019@gmail.com>
2021-11-17 03:48:17 +0100

    trabalhando com funções

---
+++ funcoes/1-funcoes.js
@@ -0,0 +1,10 @@
+function getName() {
+    return 'Cientista Fénix Avogadro';
+  }
+  
+  function logFn(fn) {
+    console.log(fn());
+  }
+  
+  const logFnResult = logFn();
+  logFnResult(getName);
